11da27e00b1bb6d846d4bfde721fcd2b14913323
Jeff Gannon <gannon345@gmail.com>
2015-07-19 15:27:35 -0600

    First Commit

--- QC/qc.py
+++ QC/qc.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 """
-Trying Human webcomic scraper by /u/tomkatt. Takes user input to specify a range of comics and downloads
+Questionable Content webcomic scraper by /u/tomkatt. Takes user input to specify a range of comics and downloads
 the images to store as .png files. Files are named after the strip number in the URL.
 """
 # For personal use only. Feel free to modify or use for non-commercial purposes. Author claims no rights to the website
-# 'tryinghuman.com', webpages therein, images, or or other information associated.
-# Tryinghuman.com and all information within is copyrighted by Emy Bitner.
+# 'questionablecontent.net', webpages therein, images, or or other information associated.
+# questionablecontent.net and all information within is copyrighted by Jeff Jacques.
 
 
 import urllib2
@@ -18,8 +18,7 @@ from bs4 import BeautifulSoup
 def try_url(url):
     """For testing if the URL is a valid page"""
     try:
-        req = urllib2.Request(url)
-        req.add_unredirected_header('User-Agent', 'http://www.reddit.com/user/tomkatt')
+        req = urllib2.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
         urllib2.urlopen(req)
         return True
     except urllib2.URLError:
@@ -29,10 +28,11 @@ def try_url(url):
 def write_file(address_url, filename):
     """Writes the file from address_url to a file, as specified by the string passed to filename"""
     if os.name == 'nt':
-        f = open("Trying Human - " + filename + ".png", 'wb')
+        f = open("Questionable Content - " + filename + ".png", 'wb')
     else:
-        f = open("Trying Human - " + filename + ".png", 'w+')
-    site = urllib2.urlopen(address_url)
+        f = open("Questionable Content - " + filename + ".png", 'w+')
+    write_req = urllib2.Request(address_url, headers={'User-Agent': 'Mozilla/5.0'})
+    site = urllib2.urlopen(write_req)
     f.write(site.read())
     f.close()
 
@@ -41,9 +41,10 @@ def comic_soup(url):
     """takes the url passed in, reads it, and uses BeautifulSoup to parse the HTML and find the portion of the HTML
     containing the end-path with the actual comic image. Returns that div to get_comics() as a string so it can be
     parsed with regex to pull the actual address value out of the string."""
-    html = urllib2.urlopen(url).read()
+    bs_req = urllib2.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    html = urllib2.urlopen(bs_req).read()
     soup = BeautifulSoup(html)
-    comicbody = soup.find("div", id="comicbody")
+    comicbody = soup.find("div", id="comic")
     return str(comicbody)
 
 
@@ -62,37 +63,26 @@ def get_comics(low_val, high_val):
         extensions = (".jpg", ".gif", ".png")
 
         for ext in extensions:
-            url = "http://tryinghuman.com/comic.php?id=" + comic_str
+            url = "http://www.questionablecontent.net/view.php?comic=" + comic_str
             url_check = try_url(url)
 
             if url_check:
                 comicbody_text = comic_soup(url)
 
-                # image_location1 = re.compile('comics/../comics/.+'+ext)
-                # image_location2 = re.compile('comics/\d+'+ext)
-
-                # image_link1 = re.search(image_location1, comicbody_text)
-                # image_link2 = re.search(image_location2, comicbody_text)
-
-                image_location = re.compile('comics/.+'+ext)
+                image_location = re.compile('/comics/.+'+ext)
                 image_link = re.search(image_location, comicbody_text)
 
                 if image_link:
-                    image_link_str = "http://tryinghuman.com/" + image_link.group()
+                    image_link_str = "http://questionablecontent.net/" + image_link.group()
                     print "Comic found. Writing comic #" + comic_str
                     write_file(image_link_str, comic_str)
-                    sleep(1.5)
+                    sleep(2)
                     break
-
-                # elif image_link2:
-                    # image_link_str = "http://tryinghuman.com/" + image_link2.group()
-                    # print "Comic found. Writing comic #" + comic_str
-                    # write_file(image_link_str, comic_str)
-                    # sleep(1.5)
-                    # break
 
                 else:
                     print "No match for comic %d on format %s" % (comic, ext)
+                    print image_location
+                    print image_link
 
             else:
                 print "no comic found for format: %s on comic string %s" % (ext, comic_str)
